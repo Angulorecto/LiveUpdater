1445e5ba34de4d889d968dfdf754563084df601d
Angulorecto <phoenixtracker305@gmail.com>
2025-07-25 10:37:55 -0400

    modified:   server.js

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const https = require("https");
 const argv = require('yargs').argv;
 
 const PLUGINS_DIR = path.resolve(__dirname, "..", "..", "plugins");
-const SERVER_PROPERTIES_PATH = path.resolve(__dirname, "..", "..", "server.properties");
+console.log(PLUGINS_DIR);
+const SERVER_PROPERTIES_PATH = path.resolve(process.cwd(), "server.properties");
 const CERT_DIR = path.resolve(__dirname, "certs");
 const CERT_PATH = path.join(CERT_DIR, "cert.pem");
 const KEY_PATH = path.join(CERT_DIR, "key.pem");
